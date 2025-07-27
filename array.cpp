99a0120a9949e6b2c779b55eb4288732ed92fc4a
RonitThakur2003 <thakurronit2003@gmail.com>
2025-07-27 16:41:36 +0530

    Array

--- array.cpp
+++ array.cpp
@@ -662,3 +662,107 @@ using namespace std;
 //         return ans;
 //     }
 // };
+
+
+// PASCAL TRIANGLE-----------------------------------------------
+
+// class Solution {
+//  public:
+//   vector<vector<int>> generate(int numRows) {
+//     vector<vector<int>> triangle;
+//     for (int i = 0; i < numRows; i++) {
+//         vector<int> row(i + 1, 1);
+//         for (int j = 1; j < i; j++) {
+//             row[j] = triangle[i - 1][j - 1] + triangle[i-1][j];
+//         }
+//         triangle.push_back(row);
+//     }
+//     return triangle;
+//     }
+// };
+
+
+// MAJORITY ELEMENTS--------------------------------------------------------
+
+// Better Approach
+
+// class Solution {
+// public:
+//     vector<int> majorityElement(vector<int>& nums) {
+//         vector<int> ans;
+//         map<int ,int> mpp;
+//         int n = nums.size();
+//         for(int i=0;i<n;i++){
+//             mpp[nums[i]]++;
+//             if(mpp[nums[i]]==n/3+1){
+//                 ans.push_back(nums[i]);
+//             }
+//         }
+//         return ans;
+//     }
+// };
+
+
+// Optimal Approach
+
+// class Solution {
+// public:
+//     vector<int> majorityElement(vector<int>& nums) {
+//         int n = nums.size();
+//         int cnt1=0,cnt2=0;
+//         int el1=INT_MIN,el2=INT_MIN;
+//         for(int i=0;i<n;i++){
+//             if(cnt1==0 && nums[i]!=el2){
+//                 el1=nums[i];
+//                 cnt1++;
+//             }else if(cnt2==0 && nums[i]!=el1){
+//                 el2=nums[i];
+//                 cnt2++;
+//             }else if(nums[i]==el1) cnt1++;
+//             else if(nums[i]==el2) cnt2++;
+//             else{
+//                 cnt1--,cnt2--;
+//             }
+//         }
+//         vector<int> ans;
+//         cnt1=0,cnt2=0;
+//         for(int i=0;i<n;i++){
+//             if(nums[i]==el1) cnt1++;
+//             else if(nums[i]==el2) cnt2++;
+//         }
+//         if(cnt1>n/3) ans.push_back(el1);
+//         if(cnt2>n/3) ans.push_back(el2);
+//         return ans;
+//     }
+// };
+
+
+// 3SUM----------------------------------------------------------
+
+// class Solution {
+// public:
+//     vector<vector<int>> threeSum(vector<int>& nums) {
+//         vector<vector<int>> ans;
+//         int n = nums.size();
+//         sort(nums.begin(),nums.end());
+//         for(int i=0;i<n;i++){
+//             if(i>0 && nums[i]==nums[i-1]) continue;
+//             int j=i+1,k=n-1;
+//             while(j<k){
+//                 int sum = nums[i]+nums[j]+nums[k];
+//                 if(sum==0){
+//                     ans.push_back({nums[i],nums[j],nums[k]});
+//                     j++;
+//                     k--;
+//                     while(j<k && nums[j]==nums[j-1]) j++;
+//                     while(j<k && nums[k]==nums[k+1]) k--; 
+//                 }else if(sum<0){
+//                     j++;
+//                 }else{
+//                     k--;                  
+//                 }
+//             }
+//         }
+//        return ans; 
+//     }
+// };
